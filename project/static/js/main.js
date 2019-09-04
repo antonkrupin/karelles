26582208c3982f824f5cd3a6d6f62b0cb9bffc19
anton <krupinanton87@gmail.com>
2019-09-04 14:50:38 +0300

    change index.html, change main.js

--- project/static/js/main.js
+++ project/static/js/main.js
@@ -1,3 +1,4 @@
+
 /*кнопки для открытия формы*/
 var button = document.querySelectorAll('.button');
 /* пункты вопросов и ответов */
@@ -15,6 +16,34 @@ var examples_img = document.querySelectorAll('.fourth_block__img .img img');
 var examples_img_popup = document.querySelector('.examples_big')
 var ex_img = document.querySelector('.examples_big__img');
 
+var menu_wrapper = document.querySelector('.menu_wrapper');
+var menu = document.querySelector('.menu');
+
+/*menu_wrapper.onmouseenter = function(){
+    menu.style.transition = '0.3s';
+    menu.style.opacity = 1;
+};
+
+menu_wrapper.onmouseleave = function(){
+    menu.style.transition = '0.3s';
+    menu.style.opacity = 0;
+};*/
+
+/*меню плавного перехода к нужному блоку*/
+$('.menu_pricing').click(function(){
+    var top_project = $('.second_block').offset().top;
+    $('body, html').animate({scrollTop: top_project}, 1000);
+});
+
+$('.menu_whoneed').click(function(){
+    var top_project = $('.third_block').offset().top;
+    $('body, html').animate({scrollTop: top_project}, 1000);
+});
+
+$('.menu_examples').click(function(){
+    var top_project = $('.fourth_block__subtitle').offset().top;
+    $('body, html').animate({scrollTop: top_project}, 1000);
+});
 
 for(i=0;i<examples_img.length;i++){
     examples_img[i].onclick = function(){
@@ -26,12 +55,14 @@ for(i=0;i<examples_img.length;i++){
         var img = ex_img.querySelector("img");
         img.setAttribute("src",src_path);
         examples_img_popup.style.display = "block";
+        menu_wrapper.style.display = 'none';
     };
 }
 /* закрытие окна с большим изображением диплома */
 if (examples_img_popup != null){
     examples_img_popup.onclick = function(){
         examples_img_popup.style.display = "none";
+        menu_wrapper.style.display = 'block';
     };
 }
 
@@ -41,10 +72,6 @@ function getCoords(elem){
     var box = elem.getBoundingClientRect();
     var res = 0;
     return res = box.top + pageYOffset - 100;
-}
-
-send_button.onclick = function(){
-    console.log("test");
 }
 
 /*блок вопросов и ответов */
@@ -58,6 +85,7 @@ for(i=0;i<question.length;i++){
 /* закрытие формы */
 from_close_button.onclick = function(){
     $(form).fadeOut(400);
+    menu_wrapper.style.display = 'block';
 };
 
 /* открытие формы при нажатии на кнопку */
@@ -66,6 +94,7 @@ for(var i = 0; i < button.length; i++){
         var form_coord = Math.round(getCoords(this));
         var top_coord = $(this).offset().top;
         $('body, html').animate({scrollTop: top_coord - 400}, 1000);
+        menu_wrapper.style.display = 'none';
         form.style.top = form_coord + 'px';
         form.style.display = "block";
     };
